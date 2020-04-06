bb3324476c5e4f12722a38fb8e7d495c7beb2ac0
glepur <goran.lepur1@gmail.com>
2020-04-06 23:27:26 +0200

    clock

--- medium/clock/src/lib.rs
+++ medium/clock/src/lib.rs
@@ -1,15 +1,38 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+  hours: i32,
+  minutes: i32,
+}
+
+impl fmt::Display for Clock {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{:02}:{:02}", self.hours, self.minutes)
+  }
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
-    }
+  pub fn new(hours: i32, minutes: i32) -> Self {
+    Clock { hours, minutes }.normalize()
+  }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+  pub fn add_minutes(&self, minutes: i32) -> Self {
+    Clock {
+      hours: self.hours,
+      minutes: self.minutes + minutes,
     }
+    .normalize()
+  }
+
+  fn normalize(&self) -> Self {
+    let (minutes, hours_offset) = euclidean_division(self.minutes, 60);
+    let (hours, _) = euclidean_division(self.hours + hours_offset, 24);
+
+    Clock { hours, minutes }
+  }
+}
+
+fn euclidean_division(n: i32, m: i32) -> (i32, i32) {
+  (n.rem_euclid(m), n.div_euclid(m))
 }
